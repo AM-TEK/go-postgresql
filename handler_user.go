43c373b0a276bf48916683003c3ae90ea2e45dcc
Alejandro Maldonado <am@AM-WEBDEV.attlocal.net>
2024-05-23 18:01:01 -0500

    add authorization with apikey

--- handler_user.go
+++ handler_user.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "net/http"
-  "time"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
 
-  "github.com/google/uuid"
-  "github.com/AM-TEK/rssagg/internal/database"
+	"github.com/AM-TEK/rssagg/internal/auth"
+	"github.com/AM-TEK/rssagg/internal/database"
+	"github.com/google/uuid"
 )
 
 func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
@@ -34,5 +35,21 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
     return
   }
 
-  respondWithJSON(w, 200, databaseUserToUser(user))
+  respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+  apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+    return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
+    return
+	}
+
+	respondWithJSON(w, 200, databaseUserToUser(user))
 }
